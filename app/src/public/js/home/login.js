ae8f3c12e7f4e12bfc8bb8deb97c9e4b89d340d6
jet981217 <jet981217@naver.com>
2021-09-25 16:54:03 +0900

    DOM

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,15 @@
 "use strirct"
 
-console.log("Hi");
+const id = document.querySelector("#id"),
+    pw = document.querySelector("#pw"),
+    loginbt = document.querySelector("button");
+
+loginbt.addEventListener("click", login);
+
+function login(){
+    const req = {
+        id : id.value,
+        pw : pw.value,
+    };
+
+}
